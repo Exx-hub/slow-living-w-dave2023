a97d92e27722b1fd7be6290835f59927acf53561
Alvin Acosta <alvin.acosta@movon.com.ph>
2023-08-19 14:29:29 +0800

    fixed location of nav menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -16,8 +16,9 @@ function Navbar() {
     const currentScrollPos = window.scrollY;
 
     setVisible(prevScrollPos > currentScrollPos || currentScrollPos < 10);
+    setMenuOpen(false);
     setPrevScrollPos(currentScrollPos);
-  }, [prevScrollPos]);
+  }, [prevScrollPos, setMenuOpen]);
 
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
@@ -38,25 +39,25 @@ function Navbar() {
       >
         <Image src="/dave-logo.png" alt="" width={80} height={40} style={{ width: "auto" }} />
         <Burger menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-      </div>
 
-      <div className={menuOpen ? "navMenu opacity-100 " : "navMenu opacity-0"}>
-        {/* white overlay  */}
-        <div className="absolute inset-0 -z-20 bg-gray-100 opacity-[85%]"></div>
-        <nav className="flex flex-col space-y-3 font-bold py-2 text-[#2d8d5c]">
-          <div className="border-b border-gray-300 px-4 pb-2 hover:text-[#005c29]">
-            <Link href="/recipes">RECIPES</Link>
-          </div>
-          <div className="border-b border-gray-300 px-4 pb-2 hover:text-[#005c29]">
-            <Link href="/about">ABOUT</Link>
-          </div>
-          <div className="border-b border-gray-300 px-4 pb-2 hover:text-[#005c29]">
-            <Link href="/merch">MERCH</Link>
-          </div>
-          <div className=" px-4 hover:text-[#005c29]">
-            <Link href="/socials">SOCIALS</Link>
-          </div>
-        </nav>
+        <div className={menuOpen ? "navMenu opacity-100 " : "navMenu opacity-0"}>
+          {/* white overlay  */}
+          <div className="absolute inset-0 -z-20 bg-gray-100 opacity-[85%]"></div>
+          <nav className="flex flex-col space-y-3 font-bold py-2 text-[#2d8d5c]">
+            <div className="border-b border-gray-300 px-4 pb-2 hover:text-[#005c29]">
+              <Link href="/recipes">RECIPES</Link>
+            </div>
+            <div className="border-b border-gray-300 px-4 pb-2 hover:text-[#005c29]">
+              <Link href="/about">ABOUT</Link>
+            </div>
+            <div className="border-b border-gray-300 px-4 pb-2 hover:text-[#005c29]">
+              <Link href="/merch">MERCH</Link>
+            </div>
+            <div className=" px-4 hover:text-[#005c29]">
+              <Link href="/socials">SOCIALS</Link>
+            </div>
+          </nav>
+        </div>
       </div>
     </header>
   );
